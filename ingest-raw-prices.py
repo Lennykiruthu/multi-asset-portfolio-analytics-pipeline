3b4296b397b585471b4d183ee3dff44ac0da0caa
lennykiruthu <kiruthulenny@gmail.com>
2026-03-14 15:25:58 +0300

    fix: hardcode DB_HOST(localhost) and DB_PORT in ingestion script

--- ingest-raw-prices.py
+++ ingest-raw-prices.py
@@ -12,8 +12,8 @@ load_dotenv()
 POSTGRES_USER     = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB       = os.getenv("POSTGRES_DB")
-DB_HOST           = os.getenv("DB_HOST")
-DB_PORT           = os.getenv("DB_PORT")
+DB_HOST           = "localhost"
+DB_PORT           = "5432"
 
 
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
